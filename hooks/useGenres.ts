184c925a0c8cf0308003d3909dad8be79344a9a5
gionny96 <gionathas@gmail.com>
2022-05-06 16:26:00 +0200

    (perf): improve useGenres hook

--- hooks/useGenres.ts
+++ hooks/useGenres.ts
@@ -1,11 +1,14 @@
 import { TmdbContext } from "context/TmdbContext";
 import { getMovieGenresFromIds } from "lib/api/genre-api";
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 
 const useGenres = (genre_ids: number[]): string[] => {
   const { genresMap } = useContext(TmdbContext);
+  const genres = useMemo(() => {
+    return getMovieGenresFromIds(genre_ids, genresMap);
+  }, [genre_ids, genresMap]);
 
-  return getMovieGenresFromIds(genre_ids, genresMap);
+  return genres;
 };
 
 export default useGenres;
